69ad46fd1b63b069025cf7302b4943dde380900f
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2024-11-07 12:04:59 +0100

    edit:HomeController extends from abstractController and render home idex twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,16 +2,16 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController
+class HomeControllerextends extends AbstractController
 {
     #[Route("/", name: "home")]
     public function index(Request $request):Response
     {
-        return new Response('Bonjour '.$request->query->get('name','anonyme'));
-
+        return $this->render('home.index');
     }
 }
