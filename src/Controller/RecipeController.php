789d277b877574861ed08c27ba29de76a744af20
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2024-11-07 12:18:21 +0100

    edit:RecipeController route naming changed

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -9,13 +9,13 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class RecipeController extends AbstractController
 {
-    #[Route('/recette', name: 'recipe.index' )]
+    #[Route('/recettes', name: 'recipe.index' )]
     public function index(Request $request): Response
     {
         return $this->render('recipe/index.html.twig');
     }
 
-    #[Route('/recette/{slug}-{id}', name: 'recipe.show', requirements: ['slug'=>'[a-z0-9-]+','id'=>'\d+'])]
+    #[Route('/recettes/{slug}-{id}', name: 'recipe.show', requirements: ['slug'=>'[a-z0-9-]+','id'=>'\d+'])]
     public function show(Request $request,string $slug,int $id): Response
     {
       return $this->render('recipe/show.html.twig',['slug'=>$slug,'id'=>$id]);
