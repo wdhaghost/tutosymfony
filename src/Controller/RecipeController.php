b2ae0ecb8e4273d9c90fc76b234d99cb4d28b3d0
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2024-11-07 11:04:59 +0100

    edit: RecipeController methods index and show

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -3,16 +3,22 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class RecipeController extends AbstractController
 {
-    #[Route('/recipe/', name: 'app_recipe')]
-    public function index(): Response
+    #[Route('/recettes', name: 'recipe.index' )]
+    public function index(Request $request): Response
     {
-        return $this->render('recipe/index.html.twig', [
-            'controller_name' => 'RecipeController',
-        ]);
+        return $this->render('recipe/index.html.twig');
+    }
+    #[Route('/recette/{slug}-{id}', name: 'recipe.show', requirements: ['slug'=>'[a-z0-9]+','id'=>'\d+'])]
+    public function show(Request $request,string $slug,int $id): Response
+    {
+
+
+      return new Response('Recette : '.$slug.' '.$id);
     }
 }
