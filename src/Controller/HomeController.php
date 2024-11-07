b393510115c9760d1d104de1d2f697f9397ddbaf
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2024-11-07 12:10:24 +0100

    fix: error in controller name

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,11 +7,11 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeControllerextends extends AbstractController
+class HomeController extends AbstractController
 {
     #[Route("/", name: "home")]
     public function index(Request $request):Response
     {
-        return $this->render('home.index');
+        return $this->render('home/index.html.twig');
     }
 }
